8df88b64673be9344f2c93cf77fe7442a824c934
changyan1990 <2452776312@qq.com>
2018-02-06 13:57:03 +0800

    附近门店样式

--- src/pages/nearShop.vue
+++ src/pages/nearShop.vue
@@ -1,9 +1,14 @@
 <template>
   <div>
     <div class="padding-10-10-15">
-      沈阳赛特综合奥莱
-      <mt-badge class="float-right" size="small" type="primary" color="#2196f3">
-        库存30</mt-badge>
+      <div>
+        <div class="float-left">沈阳赛特综合奥莱</div>
+        <mt-badge class="float-right" size="small" type="primary" color="#2196f3">
+          库存30</mt-badge>
+      </div>
+      <div>
+        dddd
+      </div>
     </div>
   </div>
 </template>
@@ -16,3 +21,6 @@
       }
   }
 </script>
+<style scoped>
+
+</style>
