bb806a0cf04e88dc2a1be8eb7cf46361847a4512
chenwenjin888 <253452632@qq.com>
2018-04-27 18:04:00 +0800

    新增加载更多操作

--- src/pages/productList.vue
+++ src/pages/productList.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="height-100">
-    <div class="dis-flex height-100-bg-eee flex-wrap"
-         v-infinite-scroll="loadMore"
-         infinite-scroll-disabled="loading"
-         infinite-scroll-distance="0">
+    <div class="dis-flex height-100-bg-eee flex-wrap">
       <div v-for="item in productList" class="width-50 padding-0-5 box-sizing" @click="goIndex(item)">
         <img :src="item.imageUrl | noSmallImg" :onerror="noFindSmallImg" alt="">
         <div class="padding-left-20 bg-fff margin-bottom-10">
@@ -16,28 +13,29 @@
         </div>
       </div>
     </div>
+    <div class="load-more" v-show="showLoadMore" @click="loadMore()">加载更多...</div>
+    <div class="load-more" v-show="showLoading">数据加载中...</div>
   </div>
 </template>
 <script>
-  import Vue from "vue";
-  import {InfiniteScroll} from 'mint-ui';
-  Vue.use(InfiniteScroll);
   import {mapGetters} from 'vuex';
   export default {
     data(){
       return {
-        pageSize: 20,
+        showLoadMore: false,
+        showLoading: false,
         params: {//请求参数
           pageNo: 1,
-          itemNo:this.$route.query.itemNo,
-          brandNo:this.$route.query.brandNo,
-          shopNo:this.$route.query.shopNo,
-          years:this.$route.query.years,
-          categoryNo:this.$route.query.categoryNo,
-          season:this.$route.query.season,
-          zoneNo:this.$route.query.zoneNo,
-          gender:this.$route.query.gender,
-          organTypeNo:this.$route.query.organTypeNo,
+          pageSize: 10,
+          itemNo: this.$route.query.itemNo,
+          brandNo: this.$route.query.brandNo,
+          shopNo: this.$route.query.shopNo,
+          years: this.$route.query.years,
+          categoryNo: this.$route.query.categoryNo,
+          season: this.$route.query.season,
+          zoneNo: this.$route.query.zoneNo,
+          gender: this.$route.query.gender,
+          organTypeNo: this.$route.query.organTypeNo,
         },
       }
     },
@@ -48,6 +46,7 @@
       }
     },
     created(){
+      this.getProductList();
     },
     methods: {
       goIndex(obj){
@@ -56,23 +55,33 @@
           query: obj
         })
         let that = this;
-        this.$store.dispatch('getProductInfo', {obj}).then(() => {
+        this.$store.dispatch('getProductInfo', { obj }).then(() => {
           that.$store.dispatch('getImgs');
         }).then(() => {
-          that.$store.dispatch('getSize', {obj});
+          that.$store.dispatch('getSize', { obj });
         }).then(() => {
           that.$store.dispatch('getRecommend');
         })
       },
       loadMore() {
-       /* this.params.pageNo++;*/
-        let that = this;
-        this.$store.dispatch("getProductList", this.params).then(()=>{
-          if (that.params.pageNo * that.params.pageSize >= that.$store.state.productList.total) {
-            this.loading = true;
-          }
-        });
+        this.showLoadMore = false;
+        this.showLoading = true;
+        this.params.pageNo++;
+        this.getProductList();
       },
+      getProductList(){
+        let that = this;
+        this.$store.dispatch("getProductList", this.params).then(() => {
+          console.log(that.params.pageNo * that.params.pageSize + ' ' +
+            that.$store.state.productList.total)
+          setTimeout(() => {
+            this.showLoading = false;
+            if (that.params.pageNo * that.params.pageSize < that.$store.state.productList.total) {
+              that.showLoadMore = true;
+            }
+          }, 100)
+        });
+      }
     },
     filters: {
       noSmallImg(value){
@@ -86,5 +95,12 @@
     display: block;
     max-width: 100%;
     max-height: 100%;
+  }
+
+  .load-more {
+    background-color: #efeff4;
+    height: 42px;
+    line-height: 42px;
+    text-align: center;
   }
 </style>
